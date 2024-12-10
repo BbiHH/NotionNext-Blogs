bb7e4b7041328b353270b39d9c560b79b1be2b8c
bbihh <wbhe00@gmail.com>
2024-12-11 00:42:23 +0800

    改完

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,13 +8,7 @@ export default function PoweredBy(props) {
   return (
     <div
       className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className}`}>
-      <span>Powered by</span>
-      <a
-        href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
-      .
+        多多留言给博主动力.
     </div>
   )
 }
